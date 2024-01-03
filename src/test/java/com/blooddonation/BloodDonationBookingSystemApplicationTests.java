cc705adf46afd1252552bc395254aea17d1ca17a
Yeji-Yoon <yoonyeji12@naver.com>
2024-01-03 18:39:59 +0900

    Initial Upload

---
+++ src/test/java/com/blooddonation/BloodDonationBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blooddonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
