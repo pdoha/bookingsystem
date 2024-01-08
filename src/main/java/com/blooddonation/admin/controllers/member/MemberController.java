1fb5d819f01b474115d0bc8a63b13f310da23655
Yeji-Yoon <yoonyeji12@naver.com>
2024-01-08 17:08:09 +0900

    Initial Upload

--- src/main/java/com/blooddonation/admin/controllers/member/MemberController.java
+++ src/main/java/com/blooddonation/admin/controllers/member/MemberController.java
@@ -4,14 +4,16 @@ package com.blooddonation.admin.controllers.member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
     @GetMapping("/join")
-    public String join() {
+    @ResponseBody
+    public void join() {
 
-        return "front/member/join";
+
     }
 }
