374c9777fd327a8e611303564aaa362efe50b62a
pdoha <thdus5147@naver.com>
2024-01-11 08:58:50 +0900

    로그인인증 & 인가 성공시 실패시 페이지 회원가입 & 검증 완료 로그인하면 아이디비번 불일치 오류

--- src/main/java/com/bloodDonation/member/service/LoginFailureHandler.java
+++ src/main/java/com/bloodDonation/member/service/LoginFailureHandler.java
@@ -1,7 +1,7 @@
 package com.bloodDonation.member.service;
 
 import com.bloodDonation.commons.Utils;
-import com.blooddonation.member.MemberUtil;
+import com.bloodDonation.member.MemberUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
