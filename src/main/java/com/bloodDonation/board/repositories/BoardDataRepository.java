4797bcc5368f4efbfce4f882084300054d81a6b0
Yeji-Yoon <yoonyeji12@naver.com>
2024-01-11 14:51:50 +0900

    페이지 송출

---
+++ src/main/java/com/bloodDonation/board/repositories/BoardDataRepository.java
@@ -0,0 +1,7 @@
+package com.bloodDonation.board.repositories;
+
+import org.choongang.board.entities.BoardData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardDataRepository extends JpaRepository<BoardData, Long> {
+}
