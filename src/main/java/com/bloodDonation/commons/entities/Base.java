e9b87ac12516318a52a79629427f70b32bdd96c9
Yeji-Yoon <yoonyeji12@naver.com>
2024-01-11 09:20:30 +0900

    오류 수정

--- src/main/java/com/bloodDonation/commons/entities/Base.java
+++ src/main/java/com/bloodDonation/commons/entities/Base.java
@@ -11,16 +11,19 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Getter @Setter
+@Getter
+@Setter
 @MappedSuperclass
+//부모 클래스는 테이블과 매핑하지 않고, 오로지 부모 클래스를 상속 받는 자식 클래스에게 부모 클래스가 가지는 칼럼만 매핑정보로 제공
 @EntityListeners(AuditingEntityListener.class)
-public abstract class Base {
+public abstract class Base {//자동 저장
+    //공통으로 사용할 속성
 
     @CreatedDate
     @Column(updatable = false) //수정 불가능하게
     private LocalDateTime createdAt; //가입일자
 
     @LastModifiedDate
-    @Column(insertable = false) //수정만 가능하게
+    @Column(insertable = false) //삽입만 가능하게
     private LocalDateTime modifiedAt; //수정일자
 }
