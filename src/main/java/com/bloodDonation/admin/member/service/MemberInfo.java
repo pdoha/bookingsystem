d58a846c4e8b4a5fb214bb4b76611b0ee33c0b53
pdoha <thdus5147@naver.com>
2024-01-19 00:06:15 +0900

    관리자페이지 회원쪽 템플릿 수정중 폴더명 변경

---
+++ src/main/java/com/bloodDonation/admin/member/service/MemberInfo.java
@@ -0,0 +1,11 @@
+//package com.bloodDonation.admin.member.service;
+//
+//import lombok.Builder;
+//import lombok.Data;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//@Data
+//@Builder
+//public class MemberInfo implements UserDetails {
+//    //외부에서 쓸 수 있게 세팅
+//}
