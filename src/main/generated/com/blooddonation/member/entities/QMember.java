c51381fecdaea9c43cbd78e1222f170a8abaf13b
jungdonghwa09 <fgdgsg09@naver.com>
2024-01-24 14:50:08 +0900

    설문-저장 작업중

--- src/main/generated/com/blooddonation/member/entities/QMember.java
+++ src/main/generated/com/blooddonation/member/entities/QMember.java
@@ -1,4 +1,4 @@
-package com.blooddonation.member.entities;
+package com.bloodDonation.member.entities;
 
 import static com.querydsl.core.types.PathMetadataFactory.*;
 
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -15,33 +16,41 @@ import com.querydsl.core.types.Path;
 @Generated("com.querydsl.codegen.DefaultEntitySerializer")
 public class QMember extends EntityPathBase<Member> {
 
-    private static final long serialVersionUID = 1278632738L;
+    private static final long serialVersionUID = -1357030654L;
 
     public static final QMember member = new QMember("member1");
 
-    public final com.blooddonation.commons.entities.QBase _super = new com.blooddonation.commons.entities.QBase(this);
+    public final com.bloodDonation.commons.entities.QBase _super = new com.bloodDonation.commons.entities.QBase(this);
 
-    public final StringPath birth = createString("birth");
+    public final StringPath address = createString("address");
+
+    public final StringPath addressSub = createString("addressSub");
+
+    public final ListPath<Authorities, QAuthorities> authorities = this.<Authorities, QAuthorities>createList("authorities", Authorities.class, QAuthorities.class, PathInits.DIRECT2);
 
     public final StringPath bldType = createString("bldType");
+
+    public final StringPath bldType2 = createString("bldType2");
 
     //inherited
     public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
 
     public final StringPath email = createString("email");
 
+    public final StringPath gid = createString("gid");
+
     public final StringPath mName = createString("mName");
 
     //inherited
     public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
-
-    public final StringPath phone = createString("phone");
 
     public final StringPath userId = createString("userId");
 
     public final NumberPath<Long> userNo = createNumber("userNo", Long.class);
 
     public final StringPath userPw = createString("userPw");
+
+    public final StringPath zonecode = createString("zonecode");
 
     public QMember(String variable) {
         super(Member.class, forVariable(variable));
